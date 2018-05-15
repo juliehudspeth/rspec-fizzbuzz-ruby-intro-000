f413968f853666c1b3b1dcbdd4ba0348e7988a43
Julie <juliejhudspeth@gmail.com>
2018-05-15 02:35:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,9 @@ def fizzbuzz (int)
   if int % 3 == 0
     puts "Fizz"
   elsif int % 5 == 0
-    puts nil
+    puts "Buzz"
+  elsif int % 3 == 0 && int % 5 ==0
+    puts "FizzBuzz"
   else
-    puts "ArgumentError"
+    puts nil
 end
